82eb914e3e12c78295cc08792d1f9cd78be7c0df
mingjianzhangzhangjianming <734456769@qq.com>
2021-07-08 18:22:12 +0800

    表单页面

--- src/view/form/basic.js
+++ src/view/form/basic.js
@@ -1,7 +1,107 @@
 import { Component } from 'react'
+import { Typography, Form, Input, InputNumber, DatePicker, Select, Radio, Button } from 'antd'
+import '@/assect/style/form/basic.less'
+
+const { Title, Paragraph } = Typography
+const { RangePicker } = DatePicker
+const { Option } = Select
+const { TextArea } = Input
+const formLayout = {
+    labelCol: {
+        sm: {
+            span: 3,
+            offset: 3
+        },
+        xs: {
+            offset: 0,
+            span: 24
+        }
+    },
+    wrapperCol: {
+        sm: {
+            span: 12
+        },
+        xs: {
+            span: 24
+        }
+    }
+}
 
 export default class BasicForm extends Component {
+    state = {}
+
     render() {
-        return <div>基础表单</div>
+        return (
+            <div className="form-content">
+                <div className="header">
+                    <Title level={3}>基础表单</Title>
+                    <Paragraph className="header-text">
+                        表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。
+                    </Paragraph>
+                </div>
+                <div className="basic-form">
+                    <div className="content">
+                        <Form {...formLayout}>
+                            <Form.Item label="标题">
+                                <Input placeholder="请输入标题" />
+                            </Form.Item>
+                            <Form.Item label="起止日期">
+                                <RangePicker style={{ width: '100%' }} />
+                            </Form.Item>
+                            <Form.Item label="目标描述">
+                                <TextArea />
+                            </Form.Item>
+                            <Form.Item label="衡量标准">
+                                <TextArea />
+                            </Form.Item>
+                            <Form.Item label="地点" name="address" initialValue={'2'}>
+                                <Select>
+                                    <Option value="1">地点1</Option>
+                                    <Option value="2">地点2</Option>
+                                    <Option value="3">地点3</Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item label="邀评人" name="invitees" initialValue="2">
+                                <Select>
+                                    <Option value="1">jack</Option>
+                                    <Option value="2">kaka</Option>
+                                    <Option value="3">coco</Option>
+                                    <Option value="4">kiki</Option>
+                                    <Option value="5">sunmi</Option>
+                                    <Option value="6">tonmi</Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item label="权重">
+                                <InputNumber
+                                    defaultValue={100}
+                                    min={0}
+                                    max={100}
+                                    formatter={value => `${value}%`}
+                                    parser={value => value.replace('%', '')}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                label="目标公开"
+                                name="targetOpen"
+                                initialValue={2}
+                                extra="客户、邀评人默认被分享"
+                            >
+                                <Radio.Group>
+                                    <Radio value={1}>公开</Radio>
+                                    <Radio value={2}>部分公开</Radio>
+                                    <Radio value={3}>不公开</Radio>
+                                </Radio.Group>
+                            </Form.Item>
+                            <Form.Item wrapperCol={{ sm: { offset: 6, span: 12 }, xs: { span: 24 } }}>
+                                <Button style={{ marginRight: 24 }}>关闭</Button>
+                                <Button type="primary" htmlType="submit">
+                                    提交
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
--- src/view/form/step.js
+++ src/view/form/step.js
@@ -1,9 +1,11 @@
 import { Component } from 'react'
-import { Typography, Steps, Form, Input, Button, message } from 'antd'
+import { Typography, Divider, Steps, Form, Input, Select, Button, Result } from 'antd'
+import { InfoCircleOutlined, CloseOutlined } from '@ant-design/icons'
 import '@/assect/style/form/step.less'
 
 const { Title, Paragraph } = Typography
 const { Step } = Steps
+const { Option } = Select
 
 const steps = [
     {
@@ -19,12 +21,54 @@ const steps = [
         description: '转账成功'
     }
 ]
+
+const formLayout = {
+    labelCol: {
+        span: 5
+    },
+    wrapperCol: {
+        span: 15
+    }
+}
+const initForm = {
+    playUser: 'qqemail',
+    collectUser: 'play',
+    collectType: 'play',
+    collectName: 'kaka',
+    amount: 500
+}
+
 export default class StepForm extends Component {
     state = {
-        current: 0
+        current: 0,
+        isHiddleAlert: false,
+        btnLoading: false
     }
+
+    confirmPlay = () => {
+        this.setState({ isHiddleAlert: !this.state.isHiddleAlert })
+    }
+
+    nextStep = () => {
+        if (this.timer) {
+            clearTimeout(this.timer)
+        }
+        this.setState({ btnLoading: true }, () => {
+            this.timer = setTimeout(() => {
+                this.setState({
+                    current: this.state.current + 1,
+                    btnLoading: false
+                })
+            }, 1500)
+        })
+    }
+
+    prevStep = () => {
+        this.setState({ current: this.state.current - 1 })
+    }
+
     render() {
-        const { current } = this.state
+        const { current, isHiddleAlert, btnLoading } = this.state
         return (
             <div className="form-content">
                 <div className="header">
@@ -34,24 +78,180 @@ export default class StepForm extends Component {
                     </Paragraph>
                 </div>
                 <div className="step-form">
-                    <Steps className="step-menu" current={current}>
+                    <Steps className="step-menu" current={current} responsive>
                         {steps.map(item => (
                             <Step key={item.title} title={item.title} description={item.description} />
                         ))}
                     </Steps>
-                    <div className="steps-content">
-                        <Form>
-                            <Form.Item label="付款账户">
-                                <Input />
-                            </Form.Item>
-                            <Form.Item label="付款账户">
-                                <Input />
-                            </Form.Item>
-                            <Form.Item label="付款账户">
-                                <Input />
-                            </Form.Item>
-                        </Form>
-                    </div>
+                    {current === 0 && (
+                        <>
+                            <div className="steps-content">
+                                <Form {...formLayout} initialValues={initForm}>
+                                    <Form.Item
+                                        label="付款账户"
+                                        name="playUser"
+                                        rules={[{ required: true, message: 'Please input!' }]}
+                                    >
+                                        <Select allowClear>
+                                            <Option value="qqemail">73445769@qq.com</Option>
+                                        </Select>
+                                    </Form.Item>
+                                    <Form.Item label="收款账户">
+                                        <Input.Group compact style={{ display: 'flex', flexFlow: 'row noWrap' }}>
+                                            <Form.Item name="collectType" style={{ margin: 0 }}>
+                                                <Select style={{ width: 90 }}>
+                                                    <Option value="play">支付宝</Option>
+                                                    <Option value="wechat">微信</Option>
+                                                </Select>
+                                            </Form.Item>
+                                            <Form.Item
+                                                name="collectName"
+                                                style={{ flex: 1, margin: 0 }}
+                                                rules={[{ required: true, message: 'Please input!' }]}
+                                            >
+                                                <Input allowClear />
+                                            </Form.Item>
+                                        </Input.Group>
+                                    </Form.Item>
+                                    <Form.Item
+                                        label="收款人姓名"
+                                        name="collectName"
+                                        rules={[{ required: true, message: 'Please input!' }]}
+                                    >
+                                        <Input allowClear />
+                                    </Form.Item>
+                                    <Form.Item
+                                        label="转账金额"
+                                        name="amount"
+                                        rules={[{ required: true, message: 'Please input!' }]}
+                                    >
+                                        <Input prefix="￥" allowClear />
+                                    </Form.Item>
+                                    <Form.Item wrapperCol={{ sm: { offset: 5, span: 3 }, xs: { span: 24 } }}>
+                                        <Button
+                                            onClick={this.nextStep}
+                                            block
+                                            loading={btnLoading}
+                                            type="primary"
+                                            htmlType="submit"
+                                        >
+                                            下一步
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </div>
+                            <Divider />
+                            <div className="footer-text">
+                                <Title level={4}>说明</Title>
+                                <Title level={5}>转账到支付宝</Title>
+                                <Paragraph className="header-text">
+                                    如果需要，这里可以放一些关于产品的常见问题说明。如果需要，
+                                    这里可以放一些关于产品的常见问题说明。如果需要，这里可以放一些关于产品的常见问题说明。
+                                </Paragraph>
+                                <Title level={5}>转账到微信</Title>
+                                <Paragraph className="header-text">
+                                    如果需要，这里可以放一些关于产品的常见问题说明。如果需要，
+                                    这里可以放一些关于产品的常见问题说明。如果需要，这里可以放一些关于产品的常见问题说明。
+                                </Paragraph>
+                            </div>
+                        </>
+                    )}
+                    {current === 1 && (
+                        <div className="confirm-play">
+                            <div className={`alert-text ${isHiddleAlert ? 'hiddle-alert' : ''} flex-row`}>
+                                <InfoCircleOutlined />
+                                <p>确认转账后，资金将直接打入对方账户，无法退回。</p>
+                                <Button
+                                    style={{ border: 'none', color: '#00000073' }}
+                                    onClick={this.confirmPlay}
+                                    shape="circle"
+                                    size="small"
+                                    type="link"
+                                    icon={<CloseOutlined />}
+                                />
+                            </div>
+                            <div className="table-text">
+                                <dl>
+                                    <dd>
+                                        <span>付款账户：</span>
+                                        <span>eleadmin@eclouds.com</span>
+                                    </dd>
+                                    <dd>
+                                        <span>收款账户：</span>
+                                        <span level={4}>test@example.com</span>
+                                    </dd>
+                                    <dd>
+                                        <span>收款人姓名：</span>
+                                        <span level={4}>kaka</span>
+                                    </dd>
+                                    <dd>
+                                        <span>转账金额：</span>
+                                        <Title level={3}>500 元</Title>
+                                    </dd>
+                                </dl>
+                                <Divider />
+                                <Form
+                                    initialValues={{ playWord: 123456 }}
+                                    labelCol={{ span: 5 }}
+                                    wrapperCol={{ span: 15 }}
+                                >
+                                    <Form.Item
+                                        label="支付密码"
+                                        name="playWord"
+                                        rules={[{ required: true, message: 'Please input!' }]}
+                                    >
+                                        <Input.Password autoComplete="off" />
+                                    </Form.Item>
+                                    <Form.Item wrapperCol={{ sm: { span: 15, offset: 5 }, xs: { span: 24 } }}>
+                                        <Button
+                                            onClick={this.nextStep}
+                                            style={{ marginRight: 24 }}
+                                            loading={btnLoading}
+                                            type="primary"
+                                        >
+                                            下一步
+                                        </Button>
+                                        <Button onClick={this.prevStep}>上一步</Button>
+                                    </Form.Item>
+                                </Form>
+                            </div>
+                        </div>
+                    )}
+                    {current === 2 && (
+                        <div className="accounts-success">
+                            <Result
+                                status="success"
+                                title="操作成功"
+                                subTitle="预计两小时内到账"
+                                extra={[
+                                    <Button onClick={() => this.setState({ current: 0 })} type="primary" key="console">
+                                        再转一笔
+                                    </Button>,
+                                    <Button key="buy">查看账单</Button>
+                                ]}
+                            />
+                            <div className="success-text">
+                                <dl>
+                                    <dd>
+                                        <span>付款账户：</span>
+                                        <span>eleadmin@eclouds.com</span>
+                                    </dd>
+                                    <dd>
+                                        <span>收款账户：</span>
+                                        <span level={4}>test@example.com</span>
+                                    </dd>
+                                    <dd>
+                                        <span>收款人姓名：</span>
+                                        <span level={4}>kaka</span>
+                                    </dd>
+                                    <dd>
+                                        <span>转账金额：</span>
+                                        <Title level={3}>500 元</Title>
+                                    </dd>
+                                </dl>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         )
